d19baa45592df77010911083b47b92a5672e530c
OwenKung0124 <440002825@gapps.yrdsb.ca>
2025-04-22 14:21:29 -0400

    Add Hero

--- MyWorld.java
+++ MyWorld.java
@@ -1,7 +1,11 @@
 import greenfoot.*;
 
 public class MyWorld extends World {
-    public MyWorld() {
+    public MyWorld() 
+    {
         super(600, 400, 1);
+        
+        Hero hero = new Hero();
+        addObject(hero, 100, 100);
     }
 }
