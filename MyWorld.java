49f17f46c5977b8ec90f6b8361347a28d0ee8d26
OwenKung0124 <440002825@gapps.yrdsb.ca>
2025-04-30 14:20:13 -0400

    Add change color

--- MyWorld.java
+++ MyWorld.java
@@ -1,9 +1,17 @@
 import greenfoot.*;
 
 public class MyWorld extends World {
+    private int frame = 0;
+    
     public MyWorld() 
     {
         super(600, 400, 1);
+        
+        //Set initial background();
+        updateBackground();
+        
+        getBackground().setColor(Color.GREEN);
+        getBackground().fill();
         
         Hero hero = new Hero();
         addObject(hero, 100, 100);
@@ -13,6 +21,25 @@ public class MyWorld extends World {
         
         Spider spider = new Spider();
         addObject(spider, 600, 300);
+    }
+    public void act(){
+        frame++;
+        if(frame% 1 ==0) { //Change color every 60 frames
+            updateBackground();
+        }
+    }
+    
+    private void updateBackground() {
+        //Cycle through colors
+        int index = (frame/60) % 4;
+        Color bgColor;
         
+        if(index == 0) bgColor = Color.RED;
+        else if (index == 1) bgColor = Color.GREEN;
+        else if(index == 2) bgColor = Color.BLUE;
+        else bgColor = Color.ORANGE;
+        
+        getBackground().setColor(bgColor);
+        getBackground().fill();
     }
 }
